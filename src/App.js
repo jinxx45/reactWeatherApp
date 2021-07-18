8e506a81674a5cb95a42ceb07f2cdc6d55b41816
jinxx45 <www.pragat7@pec.edu>
2021-07-18 21:30:19 +0530

    api update

--- src/App.js
+++ src/App.js
@@ -62,7 +62,7 @@ export default class App extends Component {
   }
 
   getWeather = async() => {
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Washington,uk&appid=${API_KEY}`)
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Washington,uk&appid=${API_KEY}`)
 
     const response = await api_call.json();
 
@@ -87,7 +87,7 @@ export default class App extends Component {
 // api call for date and time 
   getTimeandDate =async() => {
 
-    const api_call = await fetch(`http://worldtimeapi.org/api/timezone/Asia/Kolkata`)
+    const api_call = await fetch(`https://worldtimeapi.org/api/timezone/Asia/Kolkata`)
 
     const response =  await api_call.json();
 
